542c3e865e58daae0c12484f13d6d710219f5492
afzal <a.mohamedafzal@gmail.com>
2022-11-08 00:33:39 +0800

    updated models

--- app/Models/tempUser.php
+++ app/Models/tempUser.php
@@ -8,9 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class tempUser extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'temp_users';
 
     /**
      * The attributes that are mass assignable.
