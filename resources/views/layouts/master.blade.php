4b6e30140c0c6d9181ab6d79a94a0509aa4c7d09
afzal <a.mohamedafzal@gmail.com>
2022-11-07 14:06:23 +0800

    initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!-- Header Start -->
+@include('layouts.header')
+
+<!-- Topbar Start -->
+@include('layouts.topbar')
+
+<!-- Sidebar Start -->
+@include('layouts.sidebar')
+
+<!-- content -->
+@yield('content')
+
+<!-- Footer Start -->
+@include('layouts.footer')
+
+<!-- END wrapper -->
+@include('layouts.theme')
