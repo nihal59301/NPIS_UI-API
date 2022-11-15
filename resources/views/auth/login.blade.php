cd12f3b0aad06691bbe093584061fcb8194cf39c
afzal <a.mohamedafzal@gmail.com>
2022-11-15 20:01:46 +0800

    fixed login error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,18 +19,6 @@ var onReturnCallback = function(response) {
           
     }
 </script>
-<script type="text/javascript">
-    $('#reload').click(function () {        
-        $.ajax({
-            type: 'GET',
-            url: 'reload-captcha',
-            success: function (data) {
-                // console.log(data)
-                $(".captchaimg span").html(data.captcha);
-            }
-        });
-    });
-</script>
 @endpush
 @section('content')
 @if(Session::has('message'))
@@ -38,111 +26,15 @@ var onReturnCallback = function(response) {
 @endif
 
 @php
- $site_key="6Lfto-8iAAAAAH_Z0bck7F3XtBBvPN6-Jbj44rib";
- $secret_key="6Lfto-8iAAAAAE_EqMEmYoBf-dyXWGfknA7b254N";  
+$site_key= config('services.googleCaptcha.site_key');
+ $secret_key=config('services.googleCaptcha.secret_key');
 @endphp
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{url('login')}}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('No Kad Pengenalan') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Kata Laluan') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mt-2 mb-2">
-                            <div class="g-recaptcha" data-sitekey={{$site_key}} data-callback="onReturnCallback"  name="g-recaptcha-response"></div>
-                        </div>
-
-                        <!-- <div class="row mb-3">
-                            <label for="Captchaimg" class="col-md-4 col-form-label text-md-end">{{ __('Captcha') }}</label>
-
-                            <div class="col-md-6 captchaimg">
-                                <span>{!! captcha_img() !!}</span>
-                                <button type="button" class="btn btn-danger" class="reload" id="reload">
-                                &#x21bb;
-                                </button>                                                                
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="Captcha" class="col-md-4 col-form-label text-md-end">{{ __('Sila masukan captcha diatas') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="Captcha" type="tetx" class="form-control @error('Captcha') is-invalid @enderror" name="Captcha" value="{{ old('Captcha') }}" required autocomplete="Captcha" autofocus>
-
-                                @error('Captcha')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>                         -->
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button id="login" type="submit" class="btn btn-primary" disabled>
-                                    {{ __('Log Masuk') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Lupa Kata Laluan?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-
     <section>
       <!-- Button trigger modal -->
       <div class="login_container">
+      @if($errors->any())
+          {{ implode('', $errors->all('<div>:message</div>')) }}
+      @endif
         <button
           type="button"
           class="btn btn-primary login_btn"
@@ -280,8 +172,8 @@ var onReturnCallback = function(response) {
                                              class="form-control"
                                              id="nonjps_useremail"
                                              aria-describedby="emailHelp"
-                                             placeholder="E-mel" name="nonjps_useremail" />
-                                             @error('nonjps_useremail')
+                                             placeholder="E-mel" name="email" />
+                                             @error('email')
                                           <span class="invalid-feedback" role="alert">
                                               <strong>{{ $message }}</strong>
                                           </span>
@@ -292,11 +184,11 @@ var onReturnCallback = function(response) {
                                       <input type="password"
                                              class="form-control password_eye_field"
                                              id="nonjps_userpassword"
-                                             placeholder="Kata Laluan" name="nonjps_userpassword" />
+                                             placeholder="Kata Laluan" name="password" />
                                       <button class="eye_icon">
                                           <img src="assets/images/Password eye.png" alt="" />
                                       </button>
-                                      @error('nonjps_userpassword')
+                                      @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
@@ -915,7 +807,8 @@ var onReturnCallback = function(response) {
         </div>
       </div>
     </section>
-    <script>
+    
+<script>
         let round = document.querySelector(".round");
 
 let index = document.querySelector("#index");
